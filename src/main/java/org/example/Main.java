3217f50f52fe9160429bc7c51a3bbd8402a63ca3
alensden2 <alensanjohn1999@gmail.com>
2023-05-30 01:41:48 -0300

    Code completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,12 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         S3Bucket s3Bucket = new S3Bucket();
         s3Bucket.extractKeys();
         s3Bucket.credentialGenerator();
         s3Bucket.createBucket("alen-assignment1b00930528");
+        s3Bucket.uploadHtml("alen-assignment1b00930528");
+       s3Bucket.setS3ToPublic("alen-assignment1b00930528");
     }
 }
--- src/main/java/org/example/S3Bucket.java
+++ src/main/java/org/example/S3Bucket.java
@@ -6,7 +6,10 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.PutBucketPolicyRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -16,11 +19,11 @@ public class S3Bucket {
     public static String SECRET_KEY = "";
     public static String ACCESS_KEY = "";
     public static AwsCredentials CREDENTIALS = null;
-
+    public static S3Client S3ClientGLOBAL = null;
 
     /**
      * This function extracts the access and secret keys of AWS Sandbox
-     * */
+     */
     public void extractKeys() {
         Properties properties = new Properties();
         FileInputStream fileInputStream = null;
@@ -46,19 +49,47 @@ public class S3Bucket {
     }
 
     /**
-     * Creates the credential object */
-    public void credentialGenerator(){
-        CREDENTIALS = AwsBasicCredentials.create(ACCESS_KEY,SECRET_KEY);
+     * Creates the credential object static
+     */
+    public void credentialGenerator() {
+        CREDENTIALS = AwsBasicCredentials.create(ACCESS_KEY, SECRET_KEY);
     }
 
     /**
      * Creating the bucket
-     * */
-    public String createBucket(String bucketName){
+     * reference [1]
+     */
+    public String createBucket(String bucketName) {
+        // creating the s3Client
         S3Client s3Client = S3Client.builder().region(Region.US_EAST_1).credentialsProvider(() -> CREDENTIALS).build();
         CreateBucketRequest bucketRequest = CreateBucketRequest.builder().bucket(bucketName).build();
+        // creates the bucket
         s3Client.createBucket(bucketRequest);
-        return "Bucket "+ bucketName + " has been created!";
+        S3ClientGLOBAL = s3Client;
+        return "Bucket " + bucketName + " has been created!";
     }
 
+    /**
+     * Uploading the HTML File
+     * reference [2]
+     * */
+    public void uploadHtml(String bucketName) {
+        // places the index.html in the bucket
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder().bucket(bucketName).key("index.html").build();
+        S3ClientGLOBAL.putObject(putObjectRequest, new File("/home/cynos/DalhousieUniv/serverless/assignment/Assignment1/src/main/java/org/example/index.html").toPath());
+    }
+
+    /**
+     * Setting the bucket policy to public
+     * */
+    public void setS3ToPublic(String bucketName) {
+        S3ClientGLOBAL.putBucketPolicy(PutBucketPolicyRequest.builder().bucket(bucketName).policy("{\"Version\":\"2012-10-17\",\"Statement\":[{\"Sid\":\"PublicReadGetObject\",\"Effect\":\"Allow\",\"Principal\":\"*\",\"Action\":\"s3:GetObject\",\"Resource\":\"arn:aws:s3:::" + bucketName + "/*\"}]}").build());
+    }
 }
+
+
+/**
+ * References for the code -
+ * [1] https://www.codejava.net/aws/create-bucket-examples - Setting S3client and creating the bucket
+ * [2] https://docs.aws.amazon.com/AmazonS3/latest/userguide/example_s3_PutObject_section.html - put the html in a bucket
+ * */
